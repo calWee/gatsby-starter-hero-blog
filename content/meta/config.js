5e9575c90a35ace8e9d3476a1813d26e54bdac16
calWee <erwin.wei@gmail.com>
2019-01-16 01:31:08 -0800

    Update config.js

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,19 +1,19 @@
 module.exports = {
   siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
   shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  //siteDescription: "HeroBlog is a GatsbyJS starter.",
   siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "calweee",
+  authorTwitterAccount: "calweee",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "calweee",
+  // headerSubTitle: "presents another one GatsbyJS starter", 
 
   /* manifest.json */
   manifestName: "HeroBlog - a GatsbyJS starter",
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/calwee" },
+    { name: "twitter", url: "https://twitter.com/calweee" },
+    { name: "instagram", url: "http://instagram.com/calweee" }
   ]
 };
